cae0002efcd775167d30ae0715627c56d4e020de
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2019-11-17 16:46:56 +0700

    update

---
+++ Template/tem.py
@@ -0,0 +1,12 @@
+def getInput(fromFile=False):
+    if fromFile:
+        f = open('input', 'r')
+        return lambda :f.readline()[:-1]
+    return input
+
+
+def main(fromFile = False):
+    inp = getInput(fromFile)
+
+
+print(main(fromFile=True))
